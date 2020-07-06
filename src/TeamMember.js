50b0dbb52b6f06b94a58016ee837ba6301913575
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-05 20:52:15 -0400

    Moved edit click event listener to button

--- src/TeamMember.js
+++ src/TeamMember.js
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function TeamMember({setMemberToEdit, person}){
     return (
-        <div onClick={()=> setMemberToEdit(person)} className="member">
+        <div className="member">
             <p>{person.name}</p>
             <p>{person.email}</p>
             <p>{person.role}</p>
+            <button onClick={()=> setMemberToEdit(person)} >Edit</button>
         </div>
     );
 }
