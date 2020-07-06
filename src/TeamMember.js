9d10c723d5fcd2cb92c0bc92fa88e1eb83cf0d02
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-05 22:11:04 -0400

    Created basic styling for TeamMember component

--- src/TeamMember.js
+++ src/TeamMember.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import './team-member.css';
+
 export default function TeamMember({setMemberToEdit, person}){
     return (
         <div className="member">
-            <p>{person.name}</p>
-            <p>{person.email}</p>
+            <p className="name">{person.name}</p>
+            <p className="email">{person.email}</p>
             <p>{person.role}</p>
             <button onClick={()=> setMemberToEdit(person)} >Edit</button>
         </div>
